fedc1b1f3dec5ca2bb83dc8688d9f2648701c253
Daniel Griego <eggomygriego@gmail.com>
2024-12-23 10:13:37 -0800

    add split text node delimiter

--- src/text_node_utilities.py
+++ src/text_node_utilities.py
@@ -44,36 +44,3 @@ def split_nodes_delimiter(old_nodes, delimiter: TextTypeDelimiter, text_type: Te
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
-
-    # old implementation
-    # new_nodes = []
-    # size = len(delimiter.value)
-    # for node in old_nodes:
-    #     if node.text_type is not TextType.NORMAL:
-    #         continue
-    #
-    #     inc = 0
-    #
-    #     while True:
-    #         first_d = node.text.find(delimiter.value, inc)
-    #         second_d = node.text.find(delimiter.value, first_d + size)
-    #
-    #         if first_d == inc and second_d > -1:
-    #             text = node.text[first_d + size : second_d]
-    #             new_nodes.append(TextNode(text, text_type))
-    #             inc = second_d + size
-    #             continue
-    #
-    #         if first_d >= 0 and second_d == -1:
-    #             first_d = node.text.find(delimiter.value, first_d + size)
-    #
-    #         text = node.text[inc:]
-    #         if first_d > -1 and second_d > -1:
-    #             text = node.text[inc:first_d]
-    #             inc = first_d
-    #
-    #         if text:
-    #             new_nodes.append(TextNode(text, TextType.NORMAL))
-    #
-    #         if inc >= len(node.text) or first_d < 0:
-    #             break
